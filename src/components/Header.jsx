0a664718b83868f204cd5409499ad7d066acd36a
Morozov Vlad <m.uladzislau.m@gmail.com>
2023-08-08 16:41:25 +0300

    создание приложения

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Header.module.css";
+
+const header = () => {
+  return (
+    <menu>
+      <ul>
+        <li>Пункт 1</li>
+        <li>Пункт 2</li>
+        <li>Пункт 3</li>
+        <li>Пункт 4</li>
+      </ul>
+    </menu>
+  );
+};
+
+export default header;
